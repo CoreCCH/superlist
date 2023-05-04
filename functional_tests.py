c183f976a7ea708ff71ae1172b3808b049b814b9
CoreCCH <j1618james.cm05g@nctu.edu.tw>
2023-05-05 02:18:31 +0800

    First FT, using uniitest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'successfully' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith 聽到一個很酷的代辦事項app
+        # 去察看首頁
+        self.browser.get('http://localhost:8000')
+
+        # 她發現網頁標題與標頭顯示待辦事項清單
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the Test!')
+
+        # 她馬上受邀輸入一個代辦事項
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
