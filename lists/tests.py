ecfe0b129cb8b95920090b3500370c17f1e87a1f
CoreCCH <j1618james.cm05g@nctu.edu.tw>
2023-05-08 11:19:13 +0800

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 class SmokeTest(TestCase):
@@ -12,6 +13,5 @@ class SmokeTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>')) # 希望開頭有html標籤
-        self.assertIn(b'<title>To-Do list</title>', response.content) # 希望中間某處有title標籤，並有"To-Do list"
-        self.assertTrue(response.content.endswith(b'</html>')) # 希望結束有/html標籤
+        excepted_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), excepted_html)
