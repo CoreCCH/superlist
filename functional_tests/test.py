4f8bf1ed40e5f348c5c9c3df5050156ffcbfa515
CoreCCH <j1618james.cm05g@nctu.edu.tw>
2023-05-14 15:40:23 +0800

    New URL, view and template to show list

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith 聽到一個很酷的代辦事項app
         # 去察看首頁
@@ -32,9 +37,13 @@ class NewVisitorTest(LiveServerTestCase):
         # 她在文字方塊輸入"購買孔雀羽毛"
         inputbox.send_keys('Buy peacock feathers')
 
-        # 當她按下enter時，網頁會更新，現在網頁列出
-        # "1:購買孔雀羽毛"，一個代辦事項清單項目
+        # 當她按下enter時，她會被帶到新的ULR
+        # 現在網頁列出 1: Buy peacock feathers
+        # 一個待辦清單的項目
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists.+')
+        
    
         # 此時仍然有一個文字方塊，讓她可以加入另一個項目
         # 她輸入 "使用孔雀羽毛來製作一隻蒼蠅"
@@ -44,13 +53,36 @@ class NewVisitorTest(LiveServerTestCase):
         
 
         # 網頁再次更新時，現在她的清單有這兩個項目
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn(
-            '1: Buy peacock feathers' , [row.text for row in rows]
-        )
-        self.assertIn(
-            '2: Use peacock feathers to make a fly' , [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers') #檢查這個字符串是否出現在網頁上的一個表格中
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly') #檢查這個字符串是否出現在網頁上的一個表格中
+        # 她前往那個URL -她的代辦清單仍在那裡
 
-        self.fail('Finish the Test!')
+        # 新的使用者Francis來到網站
+
+        ## 我們使用一個新的瀏覽器工作階段來確保
+        ## Edith的任何資訊都不會被cookies等機制送出
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # Francis 造訪首頁。沒有任何 Edith 的清單跡象
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Use peacock feathers to make a fly', page_text)
+
+        # Francis輸入一個新項目，做出一個新清單
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis取得自己的URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 他們滿意地上床睡覺
+        
--- lists/tests.py
+++ lists/tests.py
@@ -35,7 +35,7 @@ class SmokeTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item')
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_redirects_after_POST(self):
         request = HttpRequest()
@@ -45,7 +45,7 @@ class SmokeTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     @staticmethod
     def ignore_csrf_token(html_code):
@@ -79,12 +79,17 @@ class HomePageTest(TestCase):
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+class ListViewTest(TestCase):
 
-        request = HttpRequest()
-        response = home_page(request)
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+    # def test_displays_all_items(self):
+    #     Item.objects.create(text='itemey 1')
+    #     Item.objects.create(text='itemey 2')
+
+    #     response = self.client.get('/lists/the-only-list-in-the-world/')
+
+    #     self.assertContains(response, 'itemey 1')
+    #     self.assertContains(response, 'itemey 2')
