22375fc2260c90c57a87d88c92174831bdc0df1c
CoreCCH <j1618james.cm05g@nctu.edu.tw>
2023-05-17 16:59:24 +0800

    before deploy on server

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,12 +1,26 @@
-# from django.test import LiveServerTestCase
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import unittest
 
-# class NewVisitorTest(LiveServerTestCase):
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+            super().setUpClass()
+            cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Chrome()
 
@@ -21,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith 聽到一個很酷的代辦事項app
         # 去察看首頁
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # 她發現網頁標題與標頭顯示待辦事項清單
         self.assertIn('To-Do', self.browser.title)
@@ -66,7 +80,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Chrome()
 
         # Francis 造訪首頁。沒有任何 Edith 的清單跡象
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('Use peacock feathers to make a fly', page_text)
@@ -89,7 +103,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         #Edith 前往首頁
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         #她發現輸入方塊已妥善被至中
@@ -108,3 +122,4 @@ class NewVisitorTest(StaticLiveServerTestCase):
             512,
             delta = 10
         )
+
